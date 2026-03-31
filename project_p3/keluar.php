c154f6845c3abe1e4ba2d12a72f547707f2df818
renzkece-27 <syauqasrendy3@gmail.com>
2026-03-31 20:47:15 +0700

    Delete project_p3 directory

--- project_p3/keluar.php
+++
@@ -1,9 +0,0 @@
-<?php
-// File ini untuk logout
-session_start();     // Mulai sesi
-session_destroy();   // Hapus semua data sesi (logout)
-
-// Redirect ke halaman login
-header('Location: login.php');
-exit;
-?>
